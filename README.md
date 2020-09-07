679ac39343188d553093ef6e7f6606099a13eb28
Pierre Lemaire <pierre.lemaire@kamick.org>
2020-09-07 22:01:16 +0200

    Corrections mineurs GI/GSCOP. Passage de GI à GI-2019 en prévision nouveau format.

--- README.md
+++ README.md
@@ -7,11 +7,17 @@
 - *GI-exemples*:
   docs and examples for LaTeX-beamer theme for GI
 
+- *GI_v2019*:
+  required files for LaTeX-beamer theme for GI (v. 2019)
+
+- *GI_v2019-exemples*:
+  docs and examples for LaTeX-beamer theme for GI (v. 2019)
+
 - *GSCOP*:
   required files for LaTeX-beamer theme for G-SCOP
   
 - *GSCOP-exemples*:
-  docs and examples for LaTeX-beamer theme for GI
+  docs and examples for LaTeX-beamer theme for G-SCOP
 
 - *img-src*:
   original images (for dev purpose)
--- scripts/releases.sh
+++ scripts/releases.sh
@@ -3,6 +3,8 @@
 rm -rf release
 mkdir release
 
+
+# G-SCOP
 cd ../GSCOP
 zip -9 gscop-theme.zip *.{pdf,sty}
 mv gscop-theme.zip ../scripts/release
@@ -13,6 +15,7 @@ zip -9 gscop-theme-exemples.zip *.{tex,pdf,sty} Img/*
 mv gscop-theme-exemples.zip ../scripts/release
 cd -
 
+# GI
 cd ../GI
 zip -9 gi-theme.zip *.{pdf,sty}
 mv gi-theme.zip ../scripts/release
@@ -23,6 +26,19 @@ zip -9 gi-theme-exemples.zip *.{tex,pdf,sty} Img/*
 mv gi-theme-exemples.zip ../scripts/release
 cd -
 
+# GI 2019
+cd ../GI_v2019
+zip -9 gi-theme.zip *.{pdf,sty}
+mv gi-theme.zip ../scripts/release/gi2019-theme.zip
+cd -
 
-cp ../GI-exemples/00format-gi.pdf release/
+cd ../GI_v2019-exemples
+zip -9 gi-theme-exemples.zip *.{tex,pdf,sty} Img/*
+mv gi-theme-exemples.zip ../scripts/release/gi2019-theme-exemples.zip
+cd -
+
+
+
 cp ../GSCOP-exemples/00format-gscop.pdf release/
+cp ../GI-exemples/00format-gi.pdf release/
+cp ../GI_v2019-exemples/00format-gi.pdf release/00format-gi2019.pdf
